7bcb34c001bd090f5014ec19a40e4aa51ee5c504
JuanJoven01 <juanjov95@hotmail.com>
2025-02-19 23:33:42 +0100

    feat(controller): courses
    Add function to add user to a course

--- controllers/courses.py
+++ controllers/courses.py
@@ -120,6 +120,55 @@ class CoursesAPI(http.Controller):
             _logger.error(f"Error updating course: {str(e)}")
             return _error_response(f"Error updating course: {str(e)}", 500)
         
+    ## 🔹 [PUT] Add course to user
+    @http.route('/api/exams/courses/update/add_user', type='json', auth='public', methods=['PUT'], csrf=False)
+    def add_user_to_course(self, **kwargs):
+        """
+        Updates an existing course if the user has access.
+        """
+        try:
+            user_data = JWTAuth.authenticate_request()  # Validate JWT
+            user_id = user_data.get("user_id")
+
+            code = kwargs.get('code')
+            access_key = kwargs.get('access_key')
+
+            if not code or not access_key:
+                return _error_response('Code and Access Key are required', 400)
+
+            # Find the course
+            course = request.env['easy_exams.course'].sudo().search([
+                ('code', '=', code) # Ensure user has access
+            ], limit=1)
+
+            if not course:
+                return _error_response("Course not found", 400)
+
+            if access_key != course.access_key:
+                return _error_response('Invalid Access Key', 403)
+
+            # Add the user to the course
+            if user_id not in course.user_ids.ids :
+                course.write({'user_ids': [(4, user_id)]})
+
+            # Prepare response
+            updated_course = {
+                'id': course.id,
+                'name': course.name,
+                'description': course.description,
+                'code': course.code,
+                'access_key': course.access_key,
+            }
+
+            return _success_response(updated_course, "Course updated successfully.")
+
+        except AccessDenied:
+            return _error_response("Unauthorized: Access Denied", 401)
+        except Exception as e:
+            _logger.error(f"Error updating course: {str(e)}")
+            return _error_response(f"Error updating course: {str(e)}", 500)
+        
+
     ## 🔹 [DELETE] Delete Course
     @http.route('/api/exams/courses/delete/<int:course_id>', type='http', auth='public', methods=['DELETE'], csrf=False)
     def delete_exam_course(self, course_id, **kwargs):
